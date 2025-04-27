8648c462154b55ca7dc5dc7f5c777d27ffd7b99f
Evaldas Visockas <vievaldas@gmail.com>
2025-04-27 13:51:47 +0300

    Allow saving quotes. Do not show saved quotes in random quotes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
 const { expressjwt: jwt } = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+
+const namespace = 'https://quotes.programmersdiary.com/';
 
 const jwtCheck = jwt({
   secret: jwksRsa.expressJwtSecret({
@@ -24,6 +26,7 @@ const jwtCheck = jwt({
 
 const client = new MongoClient(process.env.MONGODB_ATLAS_QUOTES_URL);
 let quotesCollection;
+let usersCollection;
 
 connectToMongo();
 
@@ -31,6 +34,7 @@ async function connectToMongo() {
   await client.connect();
   const database = client.db('Quotes');
   quotesCollection = database.collection('Quotes');
+  usersCollection = database.collection('Users');
 }
 
 app.get('/', (_, res) => {
@@ -44,15 +48,73 @@ app.use((err, _, res, next) => {
   next(err);
 });
 
-app.get('/api/quotes/random', jwtCheck, async (_, res) => {
+app.get('/api/quotes/random', jwtCheck, async (req, res) => {
   try {
+    const email = req.auth[`${namespace}email`];
+    if (!email) {
+      return res.status(400).json({ message: 'Email is required' });
+    }
+    const user = await usersCollection.findOne({ email });
+
+    let excludedQuoteIds = [];
+    if (user && user.savedQuotes) {
+      excludedQuoteIds = user.savedQuotes.map(id => ObjectId.createFromHexString(id));
+    }
+    
     const result = await quotesCollection.aggregate([
+      { $match: { _id: { $nin: excludedQuoteIds } } },
       { $sample: { size: 5 } }
     ]).toArray();
     
     res.json(result);
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).send();
+  }
+});
+
+app.post('/api/quotes/save', jwtCheck, async (req, res) => {
+  try {
+    const email = req.auth[`${namespace}email`];
+    const { quoteId } = req.body;
+    
+    if (!email || !quoteId) {
+      return res.status(400).json({ message: 'Email and quoteId are required' });
+    }
+    
+    await usersCollection.updateOne(
+      { email },
+      { $addToSet: { savedQuotes: quoteId } },
+      { upsert: true }
+    );
+    
+    res.status(200).send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+app.get('/api/quotes/saved', jwtCheck, async (req, res) => {
+  try {
+    const email = req.auth[`${namespace}email`];
+    if (!email) {
+      return res.status(400).json({ message: 'Email is required' });
+    }
+    
+    const user = await usersCollection.findOne({ email });
+    
+    if (!user || !user.savedQuotes) {
+      return res.json([]);
+    }
+    
+    const quoteIds = user.savedQuotes.map(id => ObjectId.createFromHexString(id));
+    
+    const savedQuotes = await quotesCollection.find(
+      { _id: { $in: quoteIds } }
+    ).toArray();
+    
+    res.json(savedQuotes);
+  } catch (error) {
+    res.status(500).send();
   }
 });
 
